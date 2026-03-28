04fd1f99c9311557464436467af38c3de6548b94
luizbortoluzzi <luizbortoluzzi@gmail.com>
2026-03-28 07:34:55 -0300

    fix: tests

--- frontend/src/hooks/use-is-mobile.ts
+++ frontend/src/hooks/use-is-mobile.ts
@@ -1,6 +1,7 @@
 import { useMediaQuery } from '@mantine/hooks'
 
 export const SM_BREAKPOINT = '(max-width: 48em)'
+export const MD_BREAKPOINT = '(max-width: 62em)'
 
 export function useIsMobile(): boolean {
   return useMediaQuery(SM_BREAKPOINT) ?? false
